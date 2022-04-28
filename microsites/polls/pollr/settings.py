0a3ffc58d96fe12ac7ea13fa7db52492b0de1a5c
Jason <thebosssg19@gmail.com>
2022-04-28 16:11:52 -0400

    fixed debug static dir

--- microsites/polls/pollr/settings.py
+++ microsites/polls/pollr/settings.py
@@ -16,6 +16,13 @@ from os.path import exists
 from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEBUG = False  # todo change
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -29,7 +36,6 @@ if exists('.env'):
 
 ALLOWED_HOSTS = ['.nasoj.me', '127.0.0.1']
 
-DEBUG = True  # todo change
 
 # Application definition
 
@@ -126,6 +132,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
